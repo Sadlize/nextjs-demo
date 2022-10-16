8fd41d8ad7a460a354a38d28ba47acb5f9aad5ca
Sadlize <sadlize@mail.ru>
2022-10-16 12:53:34 +0300

    feat: add postcss-simple-vars

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  "plugins": [
+  plugins: [
     "postcss-nested",
+    "postcss-simple-vars",
     "postcss-flexbugs-fixes",
     [
       "postcss-preset-env",
       {
-        "autoprefixer": {
-          "flexbox": "no-2009"
+        autoprefixer: {
+          flexbox: "no-2009",
         },
-        "stage": 3,
-        "features": {
-          "custom-properties": false
-        }
-      }
-    ]
-  ]
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
+  ],
 }
